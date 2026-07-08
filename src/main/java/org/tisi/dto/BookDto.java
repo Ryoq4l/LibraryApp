ac414c805b914d4541e9ecf59e95f3c08de071e5
Ryoq4l <eegor.koleda@gmail.com>
2026-07-08 17:27:37 +0300

    Добавлен DTO для Book

---
+++ src/main/java/org/tisi/dto/BookDto.java
@@ -0,0 +1,16 @@
+package org.tisi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDto {
+    private Long bookId;
+    private String title;
+    private String author;
+    private String isbn;
+    private Integer year;
+}
