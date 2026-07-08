2d2428a1b36f01b9478645cb41f342ab2f77fcbe
Ryoq4l <eegor.koleda@gmail.com>
2026-07-09 01:07:50 +0300

    реализован обход N+1 для findAllWithPatrons

--- src/main/java/org/tisi/repository/AuthorRepository.java
+++ src/main/java/org/tisi/repository/AuthorRepository.java
@@ -1,5 +1,6 @@
 package org.tisi.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.tisi.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
