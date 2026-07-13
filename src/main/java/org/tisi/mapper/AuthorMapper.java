7a56f7f6e6d037609efee99ba217aba9b991d844
Ryoq4l <eegor.koleda@gmail.com>
2026-07-13 14:59:28 +0300

    added map to convert Author entities to dtos

--- src/main/java/org/tisi/mapper/AuthorMapper.java
+++ src/main/java/org/tisi/mapper/AuthorMapper.java
@@ -6,12 +6,21 @@ import org.tisi.model.Author;
 
 @Component
 public class AuthorMapper {
-    public Author map(AuthorDto authorDto){
+    public Author map(AuthorDto authorDto) {
+        if (authorDto == null) return null;
         return Author.builder()
                 .authorName(authorDto.authorName())
                 .bio(authorDto.bio())
                 .build();
 
+    }
+
+    public AuthorDto toDto(Author entity) {
+        return AuthorDto.builder()
+                .authorId(entity.getAuthorId())
+                .authorName(entity.getAuthorName())
+                .bio(entity.getBio())
+                .build();
     }
 
 
