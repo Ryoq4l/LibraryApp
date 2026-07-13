b2ea3e25f7ea6e67c311d41ba5da1c929106d3a5
Ryoq4l <eegor.koleda@gmail.com>
2026-07-13 13:20:08 +0300

    created map entity -->dto

--- src/main/java/org/tisi/mapper/BorrowMapper.java
+++ src/main/java/org/tisi/mapper/BorrowMapper.java
@@ -10,14 +10,64 @@ import java.time.LocalDate;
 
 @Component
 public class BorrowMapper {
-    public BorrowRecord map (Book book, Patron patron) {
+    public BorrowRecord map (BorrowDto dto, Book book, Patron patron) {
+        if (dto == null) return null;
+
+        LocalDate borrowDate = dto.borrowDate() != null ? dto.borrowDate() : LocalDate.now();
+        LocalDate dueDate = dto.dueDate() != null ? dto.dueDate() : borrowDate.plusDays(14);
+
         return BorrowRecord.builder()
                 .patron(patron)
                 .book(book)
-                .borrowDate(LocalDate.now())
-                .dueDate(LocalDate.now().plusDays(14))
+                .borrowDate(borrowDate)
+                .dueDate(dueDate)
                 .returnDate(null)
                 .isReturned(false)
                 .build();
     }
+    public BorrowDto toDto(BorrowRecord entity){
+        if(entity == null) return null;
+        return BorrowDto.builder()
+                .id(entity.getId())
+                .bookId(getBookId(entity))
+                .bookTitle(getTitle(entity))
+                .bookIsbn(getIsbn(entity))
+                .bookPublicationYear(getYear(entity))
+                .patronId(entity.getId())
+                .patronName(getPatronName(entity))
+                .patronEmail(getEmail(entity))
+                .patronPhone(getPhone(entity))
+                .borrowDate(entity.getBorrowDate())
+                .dueDate(entity.getDueDate())
+                .returnDate(entity.getReturnDate())
+                .isReturned(entity.isReturned())
+
+                .build();
+    }
+    private Long getBookId(BorrowRecord entity){
+        return entity.getBook() != null ? entity.getBook().getBookId(): null;
+    }
+    private String getTitle (BorrowRecord entity){
+        return entity.getBook() != null ? entity.getBook().getTitle(): null;
+    }
+    private String getIsbn (BorrowRecord entity){
+        return entity.getBook() != null ? entity.getBook().getIsbn(): null;
+    }
+    private Integer getYear (BorrowRecord entity){
+        return entity.getBook() != null ? entity.getBook().getPublicationYear(): null;
+    }
+    private Long getPatronId (BorrowRecord entity){
+        return entity.getPatron() != null ? entity.getPatron().getPatronId(): null;
+    }
+    private String getPatronName (BorrowRecord entity){
+        return entity.getPatron() != null ? entity.getPatron().getPatronName(): null;
+    }
+    private String getEmail (BorrowRecord entity){
+        return entity.getPatron() != null ? entity.getPatron().getEmail(): null;
+    }
+    private String getPhone (BorrowRecord entity){
+        return entity.getPatron() != null ? entity.getPatron().getPhoneNumber(): null;
+    }
+
+
 }
