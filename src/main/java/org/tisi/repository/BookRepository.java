a36ecf80e593ce64650c12beccdda7e3ec8c90a3
Ryoq4l <eegor.koleda@gmail.com>
2026-07-14 01:01:26 +0300

    добавлен обход N+1 через entity graph

--- src/main/java/org/tisi/repository/BookRepository.java
+++ src/main/java/org/tisi/repository/BookRepository.java
@@ -1,5 +1,8 @@
 package org.tisi.repository;
 
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.tisi.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,7 +10,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    @EntityGraph(attributePaths = {"authors", "borrowRecords"})
+    List<Book> findAll(Specification<Book> spec);
     List<Book> findByTitleContainingIgnoreCase(String title);
 
 }
