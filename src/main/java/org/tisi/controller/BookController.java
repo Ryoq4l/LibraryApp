0789e6c3ae3a24645a3501627cb94c04870d1919
Ryoq4l <eegor.koleda@gmail.com>
2026-07-13 15:22:02 +0300

    added endpoints for get all books and search by title

--- src/main/java/org/tisi/controller/BookController.java
+++ src/main/java/org/tisi/controller/BookController.java
@@ -22,9 +22,17 @@ public class BookController {
     public void createBook(@RequestBody BookDto bookDto) {
         bookService.createBook(bookDto);
     }
+    @GetMapping
+    public List<BookDto> getAllBooks() {
+        return bookService.getAllBooks();
+    }
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
+    }
+    @GetMapping("/search")
+    public List<BookDto> searchBooksByTitle(@RequestParam String title) {
+        return bookService.searchBooksByTitle(title);
     }
 }
 
