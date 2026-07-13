cc81fd9b03620209fe71b4c23e02c7d83f4fb961
Ryoq4l <eegor.koleda@gmail.com>
2026-07-13 14:58:46 +0300

    added @Builder

--- src/main/java/org/tisi/dto/AuthorDto.java
+++ src/main/java/org/tisi/dto/AuthorDto.java
@@ -1,3 +1,7 @@
 package org.tisi.dto;
 
-public record AuthorDto(Long authorId, String authorName, String bio) {}
+import lombok.Builder;
+
+@Builder
+public record AuthorDto(Long authorId, String authorName, String bio) {
+}
