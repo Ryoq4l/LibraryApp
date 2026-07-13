bd7723980e1db3b52c2f123fb2b32c454db4e829
Ryoq4l <eegor.koleda@gmail.com>
2026-07-13 15:06:16 +0300

    added entity-->dto map

--- src/main/java/org/tisi/mapper/PatronMapper.java
+++ src/main/java/org/tisi/mapper/PatronMapper.java
@@ -6,12 +6,22 @@ import org.tisi.model.Patron;
 
 @Component
 public class PatronMapper {
-    public Patron map(PatronDto patronDto){
+    public Patron map(PatronDto patronDto) {
         return Patron.builder()
                 .patronName(patronDto.patronName())
                 .email(patronDto.email())
                 .phoneNumber(patronDto.phoneNumber())
                 .registrationDate(patronDto.registrationDate())
+                .build();
+    }
+
+    public PatronDto toDto(Patron entity) {
+        return PatronDto.builder()
+                .patronId(entity.getPatronId())
+                .patronName(entity.getPatronName())
+                .email(entity.getEmail())
+                .phoneNumber(entity.getEmail())
+                .registrationDate(entity.getRegistrationDate())
                 .build();
     }
 }
