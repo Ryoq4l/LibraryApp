a7179186a2b3c5b973477a7084394bccedb6bd76
Ryoq4l <eegor.koleda@gmail.com>
2026-07-13 15:05:51 +0300

    formatted + added @Builder

--- src/main/java/org/tisi/dto/PatronDto.java
+++ src/main/java/org/tisi/dto/PatronDto.java
@@ -1,11 +1,15 @@
 package org.tisi.dto;
 
+import lombok.Builder;
+
 import java.time.LocalDate;
 
+@Builder
 public record PatronDto(
         Long patronId,
         String patronName,
         String email,
         String phoneNumber,
         LocalDate registrationDate
-) {}
+) {
+}
