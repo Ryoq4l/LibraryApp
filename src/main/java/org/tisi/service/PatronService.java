9a8919537c2118acb5bc6f2beec662e9d428dc6c
Ryoq4l <eegor.koleda@gmail.com>
2026-07-13 15:06:54 +0300

    added getAllPatrons method realisation

--- src/main/java/org/tisi/service/PatronService.java
+++ src/main/java/org/tisi/service/PatronService.java
@@ -2,10 +2,14 @@ package org.tisi.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.tisi.dto.AuthorDto;
 import org.tisi.dto.PatronDto;
 import org.tisi.mapper.PatronMapper;
 import org.tisi.model.Patron;
 import org.tisi.repository.PatronRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -14,8 +18,14 @@ public class PatronService {
     private final PatronRepository patronRepo;
     private final PatronMapper patronMapper;
 
-    public void createPatron(PatronDto patronDto){
+    public void createPatron(PatronDto patronDto) {
         Patron patron = patronMapper.map(patronDto);
         patronRepo.saveAndFlush(patron);
+    }
+
+    private List<PatronDto> getAllPatrons() {
+        return patronRepo.findAll().stream()
+                .map(patronMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
