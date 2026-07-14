faf9301ba398d7be6b981fdbd6c0f849885f224c
Ryoq4l <eegor.koleda@gmail.com>
2026-07-14 12:53:14 +0300

    added derived query method for search by author name

--- src/main/java/org/tisi/repository/AuthorRepository.java
+++ src/main/java/org/tisi/repository/AuthorRepository.java
@@ -4,7 +4,10 @@ import org.tisi.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AuthorRepository  extends JpaRepository <Author, Long>{
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findByAuthorName(String authorName);
 
 }
