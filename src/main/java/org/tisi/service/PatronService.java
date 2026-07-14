9fed5ed3bac6ea597a55536fd2d164573b74b283
Ryoq4l <eegor.koleda@gmail.com>
2026-07-14 11:01:56 +0300

    get all patrons method modifier changed to public, added method for get patron by email

--- src/main/java/org/tisi/service/PatronService.java
+++ src/main/java/org/tisi/service/PatronService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.tisi.dto.AuthorDto;
 import org.tisi.dto.PatronDto;
 import org.tisi.mapper.PatronMapper;
+import org.tisi.model.Author;
 import org.tisi.model.Patron;
 import org.tisi.repository.PatronRepository;
 
@@ -19,13 +20,21 @@ public class PatronService {
     private final PatronMapper patronMapper;
 
     public void createPatron(PatronDto patronDto) {
+        if (patronRepo.existsByEmail(patronDto.email())) {
+            throw new RuntimeException("Patron with email " + patronDto.email() + " already exists");
+        }
         Patron patron = patronMapper.map(patronDto);
         patronRepo.saveAndFlush(patron);
     }
 
-    private List<PatronDto> getAllPatrons() {
+    public List<PatronDto> getAllPatrons() {
         return patronRepo.findAll().stream()
                 .map(patronMapper::toDto)
                 .collect(Collectors.toList());
+    }
+    public PatronDto getPatronByEmail(String email) {
+        Patron patron = patronRepo.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Patron not found with email: " + email));
+        return patronMapper.toDto(patron);
     }
 }
