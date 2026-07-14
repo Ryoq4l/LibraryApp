d4dea0e6275c81088ae02e57c119feb3c82350d7
Ryoq4l <eegor.koleda@gmail.com>
2026-07-14 14:12:25 +0300

    added named entity graph

--- src/main/java/org/tisi/model/Book.java
+++ src/main/java/org/tisi/model/Book.java
@@ -7,17 +7,22 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.HashSet;
 import java.util.Set;
+
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@NamedEntityGraph(
+        name = "book.authors",
+        attributeNodes = @NamedAttributeNode("authors")
+)
 @Entity
 @Table(name = "book")
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "book_id")
+    @Column(name = "book_id")
     private Long bookId;
 
     @Column(name = "title", nullable = false)
