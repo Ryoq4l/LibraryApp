605705f8a8f6e91c2e84bd19a87f4bc0ad5e5ba9
Ryoq4l <eegor.koleda@gmail.com>
2026-07-14 10:59:58 +0300

    added query method for find patron by email

--- src/main/java/org/tisi/repository/PatronRepository.java
+++ src/main/java/org/tisi/repository/PatronRepository.java
@@ -8,8 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PatronRepository extends JpaRepository<Patron, Long> {
-
+    Optional<Patron> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
