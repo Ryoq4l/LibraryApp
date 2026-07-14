32d182926bf72320a5ca79946f0b290a57c56bd6
Ryoq4l <eegor.koleda@gmail.com>
2026-07-14 22:56:31 +0300

    removed search by title endpoint as its already implemented in method search books, added delete and update endpoints

--- src/main/java/org/tisi/controller/BookController.java
+++ src/main/java/org/tisi/controller/BookController.java
@@ -1,7 +1,9 @@
 package org.tisi.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.tisi.dto.criteria.BookSearchCriteria;
 import org.tisi.service.BookService;
 import org.springframework.web.bind.annotation.*;
 import org.tisi.dto.BookDto;
@@ -33,11 +35,6 @@ public class BookController {
     public BookDto getBookById(@PathVariable Long bookId) {
         return bookService.getBookById(bookId);
     }
-
-    @GetMapping("/search")
-    public List<BookDto> searchBooksByTitle(@RequestParam String title) {
-        return bookService.searchBooksByTitle(title);
-    }
     @GetMapping("/by-author/{authorId}")
     public List<BookDto> getBooksByAuthorId(@PathVariable Long authorId) {
         return bookService.getBooksByAuthorId(authorId);
@@ -49,8 +46,23 @@ public class BookController {
     ) {
         return bookService.getBooksByPublicationRange(startY, endY);
     }
+    @GetMapping("/search")
+    public List<BookDto> searchBooks(@Valid @ModelAttribute BookSearchCriteria criteria) {
+        return bookService.searchBooks(criteria);
+    }
 
 //UPDATE ENDPOINT
+@PutMapping("/{bookId}")
+public BookDto updateBook(
+        @PathVariable Long bookId,
+        @RequestBody BookDto bookDto) {
+    return bookService.updateBook(bookId, bookDto);
+}
 //DELETE ENDPOINT
+@DeleteMapping("/{bookId}")
+@ResponseStatus(HttpStatus.NO_CONTENT)
+public void deleteBook(@PathVariable Long bookId) {
+    bookService.deleteBook(bookId);
+}
 }
 
